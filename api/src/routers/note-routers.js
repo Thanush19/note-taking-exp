6ad79b6ca5084cbd1ba24bc421bd679ab5e0f318
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2023-11-30 13:15:17 +0530

    added patch method in cors

--- api/src/routers/note-routers.js
+++ api/src/routers/note-routers.js
@@ -4,60 +4,90 @@ const Note = require("../models/note.js");
 const auth = require("../middlewares/auth");
 
 const router = new express.Router();
+router.patch("/notes/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["title", "content"]; // Specify the allowed fields to be updated
+
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const note = await Note.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+
+    if (!note) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((update) => (note[update] = req.body[update]));
+    await note.save();
+
+    res.send({ message: "Note updated successfully", note });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 router.post("/notes", auth, async (req, res) => {
-    const note = new Note({
-        ...req.body,
-        owner: req.user._id,
-    });
-    try {
-        await note.save();
-        res.status(201).send({ note, message: "Note Saved" });
-    } catch (e) {
-        res.status(500).send(e);
-    }
+  const note = new Note({
+    ...req.body,
+    owner: req.user._id,
+  });
+  try {
+    await note.save();
+    res.status(201).send({ note, message: "Note Saved" });
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 router.get("/notes", auth, async (req, res) => {
-    try {
-        // await req.user.populate("notes");
+  try {
+    // await req.user.populate("notes");
 
-        // res.status(200).send(req.user.notes);
+    // res.status(200).send(req.user.notes);
 
-        await req.user.populate("notes");
+    await req.user.populate("notes");
 
-        console.log(req.user.notes);
+    console.log(req.user.notes);
 
-        res.send(req.user.notes);
-    } catch (e) {
-        console.log(e);
-        res.status(500).send(e);
-    }
+    res.send(req.user.notes);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
 });
 
 router.get("/notes/:id", auth, async (req, res) => {
-    try {
-        const note = await Note.findById({ _id: req.params.id });
-        if (!note) {
-            return res.status(404).send();
-        }
-        res.send(note);
-    } catch (e) {
-        res.status(500).send();
+  try {
+    const note = await Note.findById({ _id: req.params.id });
+    if (!note) {
+      return res.status(404).send();
     }
+    res.send(note);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 router.delete("/notes/:id", auth, async (req, res) => {
-    try {
-        const note = await Note.findOneAndDelete({ _id: req.params.id });
+  try {
+    const note = await Note.findOneAndDelete({ _id: req.params.id });
 
-        if (!note) {
-            return res.status(404).send();
-        }
-        res.send({ message: "Note was deleted" });
-    } catch (e) {
-        res.status(500).send();
+    if (!note) {
+      return res.status(404).send();
     }
+    res.send({ message: "Note was deleted" });
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
