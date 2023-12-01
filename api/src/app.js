bd4ac5b45b3d925d1828cb2f6b8c24c6e2e44c62
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2023-12-01 17:40:42 +0530

    added cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,3 +1,11 @@
+const express = require("express");
+
+require("./db/mongoose.js");
+
+const noteRouter = require("./routers/note-routers.js");
+const userRouter = require("./routers/user-routers.js");
+
+const app = express();
 app.use((req, res, next) => {
   const allowedOrigins = [
     "http://localhost:5173",
@@ -24,4 +32,15 @@ app.use((req, res, next) => {
   }
 
   next();
+});
+
+app.use(express.json());
+
+app.use(noteRouter);
+app.use(userRouter);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Backend running on port ${port}`);
 });
