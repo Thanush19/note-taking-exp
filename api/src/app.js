f5ebffcc8b4cf8067380d5f182e0042d6b8377a9
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2023-12-01 17:45:00 +0530

    removed cors

--- api/src/app.js
+++ api/src/app.js
@@ -6,25 +6,16 @@ const noteRouter = require("./routers/note-routers.js");
 const userRouter = require("./routers/user-routers.js");
 
 const app = express();
+
 app.use((req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://note-client-ten.vercel.app",
-  ];
-
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    "GET, POST, PUT,PATCH, DELETE, OPTIONS"
   );
 
   if (req.method === "OPTIONS") {
