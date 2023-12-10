08d47cc5fa712f0815ab5d77f6f3239d7c15a920
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2023-12-10 20:35:19 +0530

    added stylings

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 h-[15vh] flex justify-between items-center w-screen bottom-0 fixed">
+    <div className="bg-gray-800 h-[15vh] flex justify-between items-center w-screen bottom-0 ">
       <h2 className="text-yellow-300 ml-4 font-light cursor-default">
         T-NOTES
       </h2>
